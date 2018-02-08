466d65428063fe36366a122a2981408c95316e1d
Joyce Lin <joycelin79@gmail.com>
2018-02-08 14:57:30 -0800

    update to grid layout

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import '../css/App.css';
 import data from '../logos.json';
+import Grid from 'react-css-grid'
 
 class App extends Component {
 
@@ -12,23 +13,19 @@ class App extends Component {
 
         let customers = this.state.data.map( (customer) => {
             return (
-                <tr key={customer.name}>
-                    <td>
-                        <img src={customer.image} alt={customer.name} className="img-responsive" />
-                    </td>
-                </tr>
+                <div key={customer.name}>
+                    <img src={customer.image} alt={customer.name} className="img-responsive" />
+                </div>
             )
         });
 
         return (
           <div className="App">
-              <table>
-                  <tbody>
-
+              <Grid
+                  width={320}
+                  gap={24}>
                     {customers}
-
-                  </tbody>
-              </table>
+              </Grid>
           </div>
         );
     }

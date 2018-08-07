0d079825e078d9cf75d2b878ae372d1862cdd0c9
Joyce Lin <joycelin79@gmail.com>
2018-08-06 21:28:48 -0700

    pull logos from local json file

--- src/components/App.js
+++ src/components/App.js
@@ -1,45 +1,65 @@
 import React, { Component } from 'react';
 import '../css/App.css';
 // import data from '../logos.json';
-import Grid from 'react-css-grid'
+import logos from '../team_logos.json';
+import Grid from 'react-css-grid';
+// import csv from 'csvtojson';
 
 class App extends Component {
 
     state = {
-        customers: []
+        customers: [],
+        logos: []
     };
 
-    componentDidMount() {
-        fetch('https://8bf5a572-4c03-4396-9b29-2aea34ff338f.mock.pstmn.io/logos')
-            .then((results) =>  {
-            return results.json();
-            }).then((data) => {
-            this.setState({
-                customers: data.customers
-            })
-        })
-
-    };
+    // componentDidMount() {
+    //     fetch('https://8bf5a572-4c03-4396-9b29-2aea34ff338f.mock.pstmn.io/logos')
+    //         .then((results) =>  {
+    //         return results.json();
+    //         }).then((data) => {
+    //         this.setState({
+    //             customers: data.customers
+    //         })
+    //     })
+    //
+    // };
+    //
+    // };
 
     render() {
 
-        let customers = this.state.customers.map( (customer) => {
+        // let customers = this.state.customers.map( (customer) => {
+        //     return (
+        //         <div key={customer.name}>
+        //             <img src={customer.image} alt={customer.name} className="img-responsive" />
+        //         </div>
+        //     )
+        // });
+
+        let customers = logos.map( (logo) => {
+            let thumbnailStyle = {
+                backgroundImage: `url(${logo.FIELD3})`
+            };
+
             return (
-                <div key={customer.name}>
-                    <img src={customer.image} alt={customer.name} className="img-responsive" />
+                <div key={logo.FIELD1}>
+                    <div className="thumbnail" style={ thumbnailStyle }></div>
+                    {/*<img src={logo.FIELD3} alt={logo.FIELD2} className="img-responsive" />*/}
                 </div>
             )
         });
 
         return (
           <div className="App">
-              <Grid
-                  width={320}
-                  gap={24}>
+              <div className="box">
+                  <Grid
+                      width={320}
+                      gap={24}>
 
-                    {customers}
+                        {customers}
 
-              </Grid>
+                  </Grid>
+              </div>
           </div>
         );
     }
